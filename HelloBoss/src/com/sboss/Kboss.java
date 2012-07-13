47c4fde862dcd8b8d82aa13bf04572717eab574d
Gopalakrishnan <tgopalakrishnan@ssdi.sharp.co.in>
2012-07-13 10:02:02 +0530

    committing HelloBoss code.

---
+++ HelloBoss/src/com/sboss/Kboss.java
@@ -0,0 +1,13 @@
+package com.sboss;
+
+public class Kboss {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("===print this line===");
+	}
+
+}
