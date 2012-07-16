119136c1bf9dd3ed55ec1a480e3e6bc18d8b4be7
Gopalakrishnan <tgopalakrishnan@ssdi.sharp.co.in>
2012-07-16 10:27:00 +0530

    This commit to test the pull request of Egit.

--- HelloBoss/src/com/sboss/Kboss.java
+++ HelloBoss/src/com/sboss/Kboss.java
@@ -6,6 +6,7 @@ int a=1,b=2;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("****New code added**");
+System.out.println("Adding new line for pull request");
 	}
 
 }
