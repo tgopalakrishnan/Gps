d6ed15632f339f7eeb0d14000ce4b3e5eaa43b95
mjayaprakash <mjayaprakash@ssdi.sharp.co.in>
2013-05-24 15:57:26 +0530

    sample commit

--- HelloBoss/src/com/sboss/Gopalnew.java
+++ HelloBoss/src/com/sboss/Gopalnew.java
@@ -7,7 +7,7 @@ public class Gopalnew {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("this is new user");
+   System.out.println("");
 	}
 
 }
