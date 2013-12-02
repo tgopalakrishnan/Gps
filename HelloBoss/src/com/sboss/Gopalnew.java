6ef9f202de90d3afc6c642a25f4539c32cfebcc2
Gopalakrishnan <tgopalakrishnan@ssdi.sharp.co.in>
2013-12-02 09:51:15 +0530

    Learn to do cherry-pick

--- HelloBoss/src/com/sboss/Gopalnew.java
+++ HelloBoss/src/com/sboss/Gopalnew.java
@@ -7,7 +7,7 @@ public class Gopalnew {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-   System.out.println("");
+   System.out.println("This is to test & train to do cherry-pick");
 	}
 
 }
